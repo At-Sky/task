f19c802456f2a526494c946971abbc8a4cb46dec
Klavdiy Tracevsky <klavdiy.tracevsky@gmail.com>
2023-02-17 17:38:38 +0300

    cleaned up

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,22 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "@/styles/CardList.module.scss"
 import useData from "@/hooks/useData";
 import Card from "@/components/Card";
 
 function CardList(): JSX.Element {
-    //const [prods, setProds] = useState<any>(null);
     const { data:prods } = useData('https://fakestoreapi.com/products')
-
-    // useEffect(() => {
-    //     const fetchData = async() => {
-    //         const response = await fetch('https://fakestoreapi.com/products');
-    //         const data = await response.json();
-    //         setProds(data);
-    //     };
-    //
-    //     fetchData()
-    // }, []);
-    
 
     return (
         <div className={styles.cardList}>
