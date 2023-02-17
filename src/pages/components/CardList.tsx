679eeb932a4f8977f008107d2a77188fe6824808
Klavdiy Tracevsky <klavdiy.tracevsky@gmail.com>
2023-02-17 17:22:56 +0300

    fix

--- src/pages/components/CardList.tsx
+++ src/pages/components/CardList.tsx
@@ -1,15 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "@/styles/CardList.module.scss"
 import useData from "@/pages/hooks/useData";
 import Card from "@/pages/components/Card";
-import IProd from "@/pages/components/Card"
+
 function CardList(): JSX.Element {
-    const { data:prods } = useData('https://fakestoreapi.com/products')
+    const [prods, setProds] = useState<any>(null);
+    //const { data:prods } = useData('https://fakestoreapi.com/products')
+
+    useEffect(() => {
+        const fetchData = async() => {
+            const response = await fetch('https://fakestoreapi.com/products');
+            const data = await response.json();
+            setProds(data);
+        };
+
+        fetchData()
+    }, []);
+    
 
     return (
         <div className={styles.cardList}>
-            {prods.map((prod, index) =>
-                <Card prodObj={prod} key={index}/>
+            {prods && prods.map((prod: any) =>
+                <Card prodObj={prod} key={prod.id}/>
             )}
         </div>
     )
