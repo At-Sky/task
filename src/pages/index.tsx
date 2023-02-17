173a53478620596728ce0466eb1e4c5648b803ed
Klavdiy Tracevsky <klavdiy.tracevsky@gmail.com>
2023-02-17 17:30:26 +0300

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,7 @@
-import { Inter } from '@next/font/google'
 import CardList from "@/components/CardList";
-import Card from "@/components/Card";
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
     return (
-      <>
         <CardList/>
-      </>
   )
 }
